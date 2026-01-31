7f021bc8dda57ae0b8759482274dec0ffd930a7e
UFerminJamie <111622743+UFerminJamie@users.noreply.github.com>
2026-01-30 19:33:42 -0500

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/uferminjamie.github.io/',
-
-  optimizeDeps: {
-    exclude: [
-      'three-mesh-bvh',
-      'three/addons/renderers/webgl/nodes/WebGLNodes.js',
-      'three-subdivide',
-      'web-ifc-three',
-      'web-ifc',
-      'three-bvh-csg',
-      'three-gpu-pathtracer',
-      'flow',
-      'three/addons/loaders/IFCLoader.js',
-    ],
-  },
+  base: '/',   // important for Netlify
 })
